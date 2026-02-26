3fac15f79a72c32890d62a915d0e92cc94384483
Gabriel <bielbuttendorf@gmail.com>
2026-02-26 18:56:45 -0300

    corringindo problema em prd para fazer deploy

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
 import { CurrentDay } from '../../components/CurrentDay';
 import { Header } from '../../components/Header';
 import { NextWeek } from '../../components/NextWeek';
 import { MainContainer, MainContent } from './styles';
 
 export function Main() {
-  const [cityName, setCityName] = useState('Jaragua do Sul');
-
   return (
     <MainContainer>
       <MainContent>
-        <Header cityName={cityName}/>
-        <CurrentDay cityName={cityName} />
+        <Header />
+        <CurrentDay />
         <NextWeek />
       </MainContent>
     </MainContainer>
